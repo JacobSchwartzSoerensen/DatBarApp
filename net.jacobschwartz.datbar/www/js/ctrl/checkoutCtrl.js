6fcc3f98fe6a3c597a09ac9b447e36a337d9c527
Jacob Sørensen <Jacobschwartzsoerensen@gmail.com>
2016-03-24 03:11:43 +0100

    Items stay on page-change + clearbutton
    closes #19

--- net.jacobschwartz.datbar/www/js/ctrl/checkoutCtrl.js
+++ net.jacobschwartz.datbar/www/js/ctrl/checkoutCtrl.js
@@ -2,9 +2,14 @@ angular.module('app')
 .controller('checkoutCtrl', ['$scope', '$rootScope', '$http', '$mdDialog', '$mdMedia', function ($scope, $rootScope, $http, $mdDialog, $mdMedia) {
     $rootScope.bottomToolbarTemplate = 'views/toolbar_checkout.html';
     $rootScope.bottomToolbarScope = $scope;
+    
+    $rootScope.topButtonsTemplate = 'views/top_buttons_checkout.html';
+    $rootScope.topButtonsScope = $scope;
 
     $scope.total = 0;
-    $scope.inCart = [];
+    if(!$rootScope.inCart){
+        $rootScope.inCart = [];
+    }
 
     $scope.showProductSearch = function (event) {
         $mdDialog.show({
@@ -39,7 +44,7 @@ angular.module('app')
     $scope.findProductFromBarcode = function (barcode) {
         var product;
 
-        for(var i = 0; i < $scope.products.length; i++){
+        for(var i = 0; i < $rootScope.products.length; i++){
             if($rootScope.products[i].barcode == barcode){
                 product = $rootScope.products[i];
                 break;
@@ -61,37 +66,47 @@ angular.module('app')
             return;
         }
 
-        if($scope.inCart.indexOf(product) != -1){
-            var index = $scope.inCart.indexOf(product);
-            $scope.inCart[index].amount += 1;
+        if($rootScope.inCart.indexOf(product) != -1){
+            var index = $rootScope.inCart.indexOf(product);
+            $rootScope.inCart[index].amount += 1;
         }else{
             product.amount = 1;
-            $scope.inCart.push(product);
+            $rootScope.inCart.push(product);
         }
         $scope.calculateTotal();
     };
 
     $scope.removeProduct = function (product) {
-        var index = $scope.inCart.indexOf(product);
-        $scope.inCart.splice(index, 1);
+        var index = $rootScope.inCart.indexOf(product);
+        $rootScope.inCart.splice(index, 1);
 
+        $scope.calculateTotal();
+    };
+
+    $scope.clearCart = function () {
+        $rootScope.inCart = [];
         $scope.calculateTotal();
     };
 
     $scope.calculateTotal = function () {
         var total = 0;
 
-        for(var i = 0; i < $scope.inCart.length; i++){
-            var item = $scope.inCart[i];
+        for(var i = 0; i < $rootScope.inCart.length; i++){
+            var item = $rootScope.inCart[i];
             total += (item.amount*item.priceInDKK);
         }
 
         $scope.total = total;
     };
 
+    $scope.calculateTotal();
+
     $scope.$on("$destroy", function(){
         $rootScope.bottomToolbarTemplate = "";
         $rootScope.bottomToolbarScope = null;
+
+        $rootScope.topButtonsTemplate = "";
+        $rootScope.topButtonsScope = null;
     });
 
 }]);
